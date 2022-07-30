e96524cd5ff807758b01017fc0fc838a95f69f97
xwinkX <opalyuk.e88@ukr.net>
2022-07-30 23:42:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,11 +35,10 @@ class App extends React.Component {
     return (
       <div
         style={{
-          height: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          fontSize: 40,
+          fontSize: 30,
           color: '#010101'
         }}
       > 
